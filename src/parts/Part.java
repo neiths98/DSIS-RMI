d7ef329136b2806c5133bb2f6fe569faf07e7137
nathan <nathan_santos@usp.br>
2022-05-27 15:58:31 -0300

    ADD: adicionando getters em Part e SubPart

--- src/parts/Part.java
+++ src/parts/Part.java
@@ -4,15 +4,31 @@ import java.util.UUID;
 import java.util.Vector;
 
 public class Part {
-  UUID id;
-  String name;
-  String description;
-  Vector<SubPart> subParts;
+  private UUID id;
+  private String name;
+  private String description;
+  private Vector<SubPart> subParts;
 
   Part(String name, String description, Vector<SubPart> subParts) {
     this.id = UUID.randomUUID();
     this.name = name;
     this.description = description;
     this.subParts = subParts;
+  }
+
+  public UUID getId() {
+    return this.id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public Vector<SubPart> getSubParts() {
+    return this.subParts;
   }
 }
--- src/parts/SubPart.java
+++ src/parts/SubPart.java
@@ -1,12 +1,20 @@
 package parts;
 
 public class SubPart {
-  Part part;
-  int quant;
+  private Part part;
+  private int quant;
 
   SubPart(Part part, int quant) {
     this.part = part;
     this.quant = quant;
+  }
+
+  public Part getPart() {
+    return this.part;
+  }
+
+  public int getQuant() {
+    return this.quant;
   }
 
 }
